1bf12fdf958fd3f67a1456e21e413a340ae5167e
djmattyg007 <mwgam36@gmail.com>
2013-12-12 09:35:36 +1100

    Tidied up database class.

--- parrot/libraries/database.php
+++ parrot/libraries/database.php
@@ -2,20 +2,22 @@
 
 // code credit goes to
 // https://github.com/anandkunal/ToroPHP/blob/master/examples/blog/lib/mysql.php
-class database {
-  private static $instance = NULL;
+class database
+{
+    private static $instance = null;
 
-  private function __construct() { }
-  private function __clone() { }
+    private function __construct() { }
+    private function __clone() { }
 
-  /**
-   * Gets the DB instance
-   */
-  public static function getInstance() {
-    if (!self::$instance) {
-      self::$instance = new PDO('mysql:host=' . DB_URL . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
-      self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    /**
+     * Gets the DB instance
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new PDO('mysql:host=' . DB_URL . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
     }
-    return self::$instance;
-  }
 }
